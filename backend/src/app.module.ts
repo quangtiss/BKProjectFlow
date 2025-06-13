244a05213cbb4b06616447575734a2c5c1f07917
quangtiss <quangpro0166@gmail.com>
2025-06-13 15:31:27 +0700

    Change and Hide JWT_SECRET from source

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,19 @@ import { Module } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 import { TaiKhoanModule } from './modules/tai_khoan/TaiKhoan.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { AuthGuard } from './modules/auth/guard/auth.guard';
 
 @Module({
-  imports: [TaiKhoanModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true, // rất quan trọng để có thể dùng process.env ở mọi nơi
+    }),
+    TaiKhoanModule,
+    AuthModule,],
   controllers: [AppController],
   providers: [
     AppService,
