78f8463842068a8baa6fcd9a7b9630cf6f0a033c
quangtiss <quangpro0166@gmail.com>
2025-07-02 18:02:50 +0700

    BE: DTO create and update_trang_thai on de_tai model

--- backend/src/modules/de_tai/dto/create_de_tai.dto.ts
+++ backend/src/modules/de_tai/dto/create_de_tai.dto.ts
@@ -3,14 +3,14 @@ import { Transform } from "class-transformer";
 
 export class CreateDeTaiDTO {
     // @IsOptional()
-    // @IsIn(['Thực hiện','Bảo lưu' ,'Hoàn thành'],
-    //     { message: "Trạng thái chỉ có thể là Thực hiện hoặc Hoàn thành" })
+    // @IsIn(['Chưa chấp nhận', 'Thực hiện', 'Bảo lưu', 'Hoàn thành'],
+    //     { message: "Trạng thái chỉ có thể là Chưa chấp nhận, Thực hiện, Bảo lưu hoặc Hoàn thành" })
     // trang_thai: string;
 
 
     // @IsOptional()
-    // @IsIn(['Chưa chấp nhận', 'Chưa duyệt', 'Đã duyệt'],
-    //     { message: "Trạng thái duyệt chỉ có thể là Chưa chấp nhận, Chưa duyệt hoặc Đã duyệt" })
+    // @IsIn(['Chưa duyệt', 'Đã duyệt'],
+    //     { message: "Trạng thái duyệt chỉ có thể là Chưa duyệt hoặc Đã duyệt" })
     // trang_thai_duyet: string;
 
 
