b8cf3a4c49726130c0b96dc7ffebc0ac049896bf
quangtiss <quangpro0166@gmail.com>
2025-07-08 17:41:10 +0700

    BE: create new base file to update trang_thai

---
+++ backend/src/modules/dang_ki/dto/update_dang_ki_trang_thai.dto.ts
@@ -0,0 +1,8 @@
+import { IsIn } from "class-validator"
+
+export class UpdateDangKiDTO {
+    @IsIn(['Chưa chấp nhận', 'Đã từ chối', 'Đã chấp nhận'], {
+        message: 'Trạng thái hướng dẫn chỉ có thể là Chưa chấp, Đã từ chối hoặc Đã chấp nhận'
+    })
+    trang_thai: string
+}
