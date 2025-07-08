898dee6327325ba144fe7ff900a150e9ff8244f5
quangtiss <quangpro0166@gmail.com>
2025-07-08 17:40:30 +0700

    BE: Rename from update_trang_thai to this name

---
+++ backend/src/modules/de_tai/dto/update_de_tai_trang_thaidto.ts
@@ -0,0 +1,18 @@
+import { IsIn, IsOptional } from 'class-validator';
+
+export class UpdateTrangThaiDTO {
+    @IsOptional()
+    @IsIn(['GVHD chưa chấp nhận', 'GVHD đã từ chối', 'GVHD đã chấp nhận', 'Thực hiện', 'Bảo lưu', 'Hoàn thành'],
+        { message: "Trạng thái chỉ có thể là GVHD chưa chấp nhận, GVHD đã từ chối, GVHD đã chấp nhận,  Thực hiện, Bảo lưu hoặc Hoàn thành" })
+    trang_thai: string;
+
+
+    @IsOptional()
+    @IsIn(['Chưa duyệt', 'Đã duyệt'],
+        { message: "Trạng thái duyệt chỉ có thể là Chưa duyệt hoặc Đã duyệt" })
+    trang_thai_duyet: string;
+}
+
+//Đăng nhập GG, vai_tro, import account excel
+
+//DeXuat 
