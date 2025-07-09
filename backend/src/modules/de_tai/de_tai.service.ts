6bc1b8cdd21530a85e95d16bd492595c0940389a
quangtiss <quangpro0166@gmail.com>
2025-07-09 17:24:25 +0700

    complete create de_tai, huong_dan, dang_ki service, and set agrument transaction to rollback all if one of theme get error

--- backend/src/modules/de_tai/de_tai.service.ts
+++ backend/src/modules/de_tai/de_tai.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { DangKiService } from '../dang_ki/dang_ki.service';
 import { HuongDanService } from '../huong_dan/huong_dan.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class DeTaiService {
@@ -46,34 +47,61 @@ export class DeTaiService {
 
 
     async create(data: any, user: any) {
-        const { id_giang_vien_huong_dan, ...dataDeTai } = data
+        const { id_giang_vien_huong_dan, list_id_sinh_vien_tham_gia, ...dataDeTai } = data
+        const prefix =
+            dataDeTai.nhom_nganh === "Khoa học Máy tính"
+                ? "KH"
+                : dataDeTai.nhom_nganh === "Kĩ thuật Máy tính"
+                    ? "KT"
+                    : "DN";
 
 
-        const deTai = await this.prismaService.de_tai.create({
-            data: {
-                ...dataDeTai,
-                ngay_tao: new Date(),
-                trang_thai: "Thực hiện",
-                trang_thai_duyet: "Chưa chấp nhận",
-                giai_doan: "Đồ án chuyên ngành",
-                id_tai_khoan_de_xuat: user.sub
-            }
-        })
-        await this.huongDanService.create({
-            vai_tro: "Giảng viên hướng dẫn chính",
-            id_giang_vien: id_giang_vien_huong_dan,
-            id_de_tai: deTai.id,
-            trang_thai: "Chưa chấp nhận",
-        })
-        return deTai;
+        return await this.prismaService.$transaction(async (tx) => {
+            const deTai = await tx.de_tai.create({
+                data: {
+                    ...dataDeTai,
+                    ma_de_tai: null,
+                    ngay_tao: new Date(),
+                    trang_thai: user.sub === id_giang_vien_huong_dan ? "GVHD đã chấp nhận" : "GVHD chưa chấp nhận",
+                    trang_thai_duyet: "Chưa duyệt",
+                    giai_doan: "Đồ án chuyên ngành",
+                    id_tai_khoan_de_xuat: user.sub
+                }
+            })
+            const maDeTai = `${prefix}${deTai.id.toString().padStart(4, "0")}`;
+            const deTaiUpdated = await this.update(deTai.id, { ma_de_tai: maDeTai }, tx)
+
+
+            await this.huongDanService.create({
+                vai_tro: "Giảng viên hướng dẫn chính",
+                id_giang_vien: id_giang_vien_huong_dan,
+                id_de_tai: deTai.id,
+                trang_thai: user.sub === id_giang_vien_huong_dan ? "Đã chấp nhận" : "Chưa chấp nhận",
+            }, tx)
+
+
+            await Promise.all(
+                list_id_sinh_vien_tham_gia.map((idSinhVien: number) =>
+                    this.dangKiService.create({
+                        ngay_dang_ki: new Date(),
+                        trang_thai: user.sub === idSinhVien ? "Đã chấp nhận" : "Chưa chấp nhận",
+                        id_sinh_vien: idSinhVien,
+                        id_de_tai: deTai.id
+                    }, tx)
+                )
+            )
+
+            return deTaiUpdated
+        });
     }
 
-    async update(id: number | undefined, data: any) {
+    async update(id: number | undefined, data: any, tx?: Prisma.TransactionClient) {
+        const client = tx ?? this.prismaService;
         if (!data || Object.keys(data).length === 0) {
             // Không có gì để cập nhật
             return null;
         }
-        return await this.prismaService.de_tai.update({
+        return await client.de_tai.update({
             where: { id: id },
             data: data
         })
