80e82bd02ed436a1501662cdcbbb4d62f62af625
quangtiss <quangpro0166@gmail.com>
2025-07-16 09:26:00 +0700

    rename to UpdateTrangThaiDangKyDTO

--- backend/src/modules/dang_ky/dto/update_trang_thai_dang_ky.dto.ts
+++ backend/src/modules/dang_ky/dto/update_trang_thai_dang_ky.dto.ts
@@ -1,6 +1,6 @@
 import { IsIn } from "class-validator"
 
-export class UpdateDangKiDTO {
+export class UpdateTrangThaiDangKyDTO {
     @IsIn(['Chưa chấp nhận', 'Đã từ chối', 'Đã chấp nhận'], {
         message: 'Trạng thái hướng dẫn chỉ có thể là Chưa chấp, Đã từ chối hoặc Đã chấp nhận'
     })
