749b1ddb2487c408aa82c32e80a2151b6d423110
quangtiss <quangpro0166@gmail.com>
2025-08-09 08:41:39 +0700

    import thong_bao, tuong_tac, giang_vien, sinh_vien, duyet_de_tai module

--- backend/src/modules/de_tai/de_tai.module.ts
+++ backend/src/modules/de_tai/de_tai.module.ts
@@ -4,9 +4,23 @@ import { DeTaiService } from './de_tai.service';
 import { PrismaModule } from 'prisma/prisma.module';
 import { DangKiModule } from '../dang_ky/dang_ky.module';
 import { HuongDanModule } from '../huong_dan/huong_dan.module';
+import { ThongBaoModule } from '../thong_bao/thong_bao.module';
+import { TuongTacModule } from '../tuong_tac/tuong_tac.module';
+import { GiangVienModule } from '../giang_vien/giang_vien.module';
+import { SinhVienModule } from '../sinh_vien/sinh_vien.module';
+import { DuyetDeTaiModule } from '../duyet_de_tai/duyet_de_tai.module';
 
 @Module({
-  imports: [PrismaModule, DangKiModule, forwardRef(() => HuongDanModule)],
+  imports: [
+    PrismaModule,
+    SinhVienModule,
+    GiangVienModule,
+    forwardRef(() => DangKiModule),
+    forwardRef(() => HuongDanModule),
+    forwardRef(() => DuyetDeTaiModule),
+    ThongBaoModule,
+    TuongTacModule
+  ],
   controllers: [DeTaiController],
   providers: [DeTaiService],
   exports: [DeTaiService]
