996b5f06943b57ba9cd2dd946297662f1f5f3fbe
quangtiss <quangpro0166@gmail.com>
2025-08-09 08:38:26 +0700

    change message error

--- backend/src/modules/dang_ky/dto/update_trang_thai_dang_ky.dto.ts
+++ backend/src/modules/dang_ky/dto/update_trang_thai_dang_ky.dto.ts
@@ -2,7 +2,7 @@ import { IsIn } from "class-validator"
 
 export class UpdateTrangThaiDangKyDTO {
     @IsIn(['Chưa chấp nhận', 'Đã từ chối', 'Đã chấp nhận'], {
-        message: 'Trạng thái hướng dẫn chỉ có thể là Chưa chấp, Đã từ chối hoặc Đã chấp nhận'
+        message: 'Trạng thái chỉ có thể là Chưa chấp nhận, Đã từ chối hoặc Đã chấp nhận'
     })
     trang_thai: string
 }
