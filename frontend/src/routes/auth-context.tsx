08bfdef63d76ddbff76f8114e3d11810e6a55f55
quangtiss <quangpro0166@gmail.com>
2025-08-09 08:51:28 +0700

    add notification and setNotification, refreshContext

--- frontend/src/routes/auth-context.tsx
+++ frontend/src/routes/auth-context.tsx
@@ -12,7 +12,12 @@ type AuthContextType = {
     loading: boolean;
     setIsAuthenticated: (value: boolean) => void;
     user: object | null;
-    setUser: (value: object | null) => void
+    setUser: (value: object | null) => void;
+    notifications: object | null;
+    setNotifications: (value: object | null) => void;
+    updateContext: boolean;
+    setUpdateContext: (updater: (value: boolean) => boolean) => void;
+    refreshContext: () => void;
 };
 
 const AuthContext = createContext<AuthContextType>({
@@ -20,13 +25,25 @@ const AuthContext = createContext<AuthContextType>({
     loading: true,
     setIsAuthenticated: () => { },
     user: null,
-    setUser: () => { }
+    setUser: () => { },
+    notifications: null,
+    setNotifications: () => { },
+    updateContext: false,
+    setUpdateContext: () => { },
+    refreshContext: () => { }
 });
 
 export function AuthProvider({ children }: { children: ReactNode }) {
     const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [loading, setLoading] = useState(true);
     const [user, setUser] = useState<object | null>(null);
+    const [notifications, setNotifications] = useState<object | null>(null)
+    const [updateContext, setUpdateContext] = useState(false)
+
+
+    const refreshContext = () => {
+        setUpdateContext((prev) => !prev); // toggle để useEffect chạy lại
+    };
 
     useEffect(() => {
         const isLogin = async () => {
@@ -39,6 +56,14 @@ export function AuthProvider({ children }: { children: ReactNode }) {
                     const data = await response.json()
                     setIsAuthenticated(true)
                     setUser(data)
+
+                    const response2 = await fetch('http://localhost:3000/thong-bao/count-all', {
+                        method: 'GET',
+                        credentials: 'include'
+                    })
+                    if (response2.ok) {
+                        setNotifications(await response2.json())
+                    }
                 }
                 else {
                     setUser(null)
@@ -53,10 +78,10 @@ export function AuthProvider({ children }: { children: ReactNode }) {
 
         }
         isLogin()
-    }, []);
+    }, [updateContext]);
 
     return (
-        <AuthContext.Provider value={{ isAuthenticated, loading, setIsAuthenticated, user, setUser }}>
+        <AuthContext.Provider value={{ isAuthenticated, loading, setIsAuthenticated, user, setUser, notifications, setNotifications, refreshContext }}>
             {children}
         </AuthContext.Provider>
     );
