7e69f2088253fa164c1fd0b960f75dbd378100d8
quangtiss <quangpro0166@gmail.com>
2025-09-06 17:22:01 +0700

    create service to get file from server

--- backend/src/modules/a_utils/utils.controller.ts
+++ backend/src/modules/a_utils/utils.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Request } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Res } from '@nestjs/common';
 import { UtilsService } from './utils.service';
 import { Roles } from '../auth/guard/roles.decorator';
 import { Public } from '../auth/guard/public.decorator';
+import { join } from 'path';
 
 @Controller('utils')
 export class UtilsController {
@@ -10,6 +11,11 @@ export class UtilsController {
   @Public()
   @Post('generate-topic')
   getTopicFromDescription(@Body() body) {
-    return this.utilsService.generateTopicFromDescription(body, 20)
+    return this.utilsService.generateTopicFromDescription(body, 1)
+  }
+
+  @Get('file/:id/:filename')
+  getProjectFile(@Param('id') id: string, @Res() res) {
+    return this.utilsService.getFile(+id, res)
   }
 }
--- backend/src/modules/a_utils/utils.service.ts
+++ backend/src/modules/a_utils/utils.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { join } from 'path';
 import { PrismaService } from 'prisma/prisma.service';
 
 @Injectable()
@@ -8,13 +9,33 @@ export class UtilsService {
   ) { }
 
 
+  async getFile(id, res) {
+    let file: any = await this.prismaService.tai_lieu.findUnique({
+      where: { id }
+    })
+    if (!file) {
+      file = await this.prismaService.tep_dinh_kem.findUnique({
+        where: { id }
+      })
+      if (!file) throw new NotFoundException("Không tìm thấy file")
+    }
+    const safeFileName = encodeURIComponent(file?.ten_tai_lieu || "");
+    res.setHeader(
+      'Content-Disposition',
+      `inline; filename*=UTF-8''${safeFileName}`
+    );
+    const filePath = join(process.cwd(), file?.url || "", file?.ten_tai_lieu || "");
+    return res.sendFile(filePath);
+  }
+
+
   async generateTopicFromDescription(bodyData: { mo_ta: string }, idDeTai: number) {
     const listChuDe = await this.prismaService.chu_de.findMany()
     const moTa = bodyData.mo_ta
     const response = await fetch("https://openrouter.ai/api/v1/chat/completions", {
       method: "POST",
       headers: {
-        "Authorization": "Bearer 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",
+        "Authorization": "Bearer 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",
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
@@ -25,7 +46,7 @@ export class UtilsService {
             "content": [
               {
                 "type": "text",
-                "text": `Đây là list chủ đề ${JSON.stringify(listChuDe)}, đây là mô tả của đề tài: ${moTa}
+                "text": `Đây là list chủ đề ${JSON.stringify(listChuDe)}, đây là tên tiếng anh, tiếng việt và mô tả của đề tài: ${moTa}
                 Bạn hãy phân tích mô tả sau đó xem nó thuộc các thể loại ten_chu_de nào ở trên, liệt kê thật đầy đủ, hãy liệt kê ra theo dạng chuỗi id của chúng trong array trên, các thành phần không được lặp lại và phải cách nhau dấu , . Bạn phải trả về dạng chuỗi theo yêu cầu và không làm gì hay nói gì thêm.`
               }
             ]
