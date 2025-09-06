86410180ac84a76dd67def4f0040eb7e4a94e20b
quangtiss <quangpro0166@gmail.com>
2025-09-06 17:18:19 +0700

    adjusting interface

--- frontend/src/pages/403.tsx
+++ frontend/src/pages/403.tsx
@@ -1,9 +1,24 @@
+import "@/styles/403.css"
+import { useNavigate } from "react-router-dom";
+
 export default function Forbidden() {
+    const navigate = useNavigate()
     return (
-        <div className="flex flex-1 flex-col">
-            <div className="@container/main flex flex-1 flex-col gap-2">
-                <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                    Bạn không có quyền truy cập vào đây
+        <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <div className="container">
+                    <div className="page-wrap">
+                        <div className="page-not-found">
+                            <img src="https://res.cloudinary.com/razeshzone/image/upload/v1588316204/house-key_yrqvxv.svg" className="img-key" alt="" />
+                            <h1 className="text-xl">
+                                <span>4</span>
+                                <span>0</span>
+                                <span className="broken">3</span>
+                            </h1>
+                            <h4 className="text-md">Access Denied !</h4>
+                            <h4 className="text-sm text-sm-btm"><a href="#" onClick={() => navigate(-1)}>Previous page</a></h4>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
