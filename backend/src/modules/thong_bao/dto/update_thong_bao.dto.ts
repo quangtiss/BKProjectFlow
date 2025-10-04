ba4ff1b7cd4ceb0b13b4519875799eea8c51cb1a
quangtiss <quangpro0166@gmail.com>
2025-10-04 16:29:15 +0700

    delete non use file

--- backend/src/modules/thong_bao/dto/update_thong_bao.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateThongBaoDto } from './create_thong_bao.dto';
-
-export class UpdateThongBaoDto extends PartialType(CreateThongBaoDto) { }
--- backend/src/modules/tuong_tac/dto/update_tuong_tac.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTuongTacDto } from './create_tuong_tac.dto';
-
-export class UpdateTuongTacDto extends PartialType(CreateTuongTacDto) { }
